bf48b8863a47cdda942ef4485f53800ea3f70941
priyaJainCoder10 <133470925+priyaJainCoder10@users.noreply.github.com>
2023-12-12 09:39:10 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,15 @@
+console.log("External Java Script");
+// Externally JS - Method implements 
+
+// Template Literals 
+let plan=1000;
+let services="Fleet Edge";
+console.log(`To continue using ${services} Renew plan of ${plan} or upgrade to Advanced plan to take your experience to next level`);
+
+// concatenation of two string 
+let A="Fleet"
+let B="Edge"
+console.log("Fleet" + "Edge");
+console.log(`The Fleet Edge followed by ${5*100}+ subscribers`)
+
+
